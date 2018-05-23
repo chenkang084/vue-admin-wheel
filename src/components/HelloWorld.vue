fa1b9c186cc38345343eb81552884f3fc09d282d
chenkang084 <chenkang1@lenovo.com>
2018-05-23 17:43:43 +0800

    add element ui ,normalize

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -86,17 +86,23 @@
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+  mounted() {
+    this.$notify({
+      title: '标题名称',
+    });
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
